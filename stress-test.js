a8d58c0a5ec16e6907ddbb56c53d1c258bdb5944
Darian Hogue <hogued96@gmail.com>
2022-06-07 00:43:23 -0400

    added more endpoints for stress tests. Connected k6 to newrelic

--- stress-test.js
+++ stress-test.js
@@ -3,11 +3,17 @@ import { sleep } from 'k6';
 
 export const options = {
   vus: 1,
-  duration: '30s',
+  duration: '2m',
 };
 
 export default function () {
-  http.get('http://localhost:8024/reviews?count=100&product_id=800555');
+  const getReviews = 'http://localhost:8024/reviews?product_id=904002';
+  const getReviewsCount = 'http://localhost:8024/reviews?count=100&product_id=8005';
+  const getReviewsSort = 'http://localhost:8024/reviews?sort=helpful&product_id=2210';
+  const getReviewsSortCount = 'localhost:8024/reviews?sort=helpful&count=100&product_id=872815';
+  const getReviewsMeta = 'http://localhost:8024/reviews/meta?product_id=71699';
+
+  http.get(getReviews);
   sleep(1);
 
 }
